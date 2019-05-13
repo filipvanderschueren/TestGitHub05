9b8956a396b7ff90683c752f0b8472cd25a3df08
filipvanderschueren <vanderschueren.filip@gmail.com>
2019-05-13 13:20:41 +0200

    Cless Value added

--- src/be/ua/fti/Main.java
+++ src/be/ua/fti/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello new message");
+
+        Value v = new Value(22);
+        System.out.println("Value added : " + v);
     }
 }
---
+++ src/be/ua/fti/Value.java
@@ -0,0 +1,17 @@
+package be.ua.fti;
+
+public class Value {
+    private Integer aValue;
+
+    public Value(Integer aValue) {
+        this.aValue = aValue;
+    }
+
+    public Integer getaValue() {
+        return aValue;
+    }
+
+    public void setaValue(Integer aValue) {
+        this.aValue = aValue;
+    }
+}
